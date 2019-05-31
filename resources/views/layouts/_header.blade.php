543f26663a7a3a8e4bf0e724fbfb1b153c4acb7c
宋天耀 <309833997@qq.com>
2019-05-31 05:43:32 +0000

    静态文件浏览器缓存问题

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,34 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+      LaraBBS
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><a class="nav-link" href="#">博客</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Laravel</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Linux</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">技术之外</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">教程</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">关于</a></li>
+
+      </ul>
+
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
