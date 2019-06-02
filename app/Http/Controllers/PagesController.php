2b157f7679b1c0b57d20ef9211ef9bd9b82fe2d2
宋天耀 <309833997@qq.com>
2019-06-02 05:00:17 +0000

    首页微博列表

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Status;
+use Auth;
 
 class PagesController extends Controller
 {
     public function root()
     {
-        return view('pages.root');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+        return view('pages.root', compact('feed_items'));
     }
     public function about()
     {
