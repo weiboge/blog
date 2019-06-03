be1c523c950c426d788ebb969199e700788f6789
宋天耀 <309833997@qq.com>
2019-06-03 09:01:12 +0000

    显示头像

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -2,5 +2,7 @@
 
 <a href="{{ route('users.show', $user->id) }}">
   <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
+
+
 </a>
 <h1><a href="{{route('users.information', $user->id)}}"> {{ $user->name }}</a></h1>
